8ae164016c98e0463b1ab83fbb1266f534c25d86
vsivanat <vsivanat@student.42.fr>
2024-01-23 22:07:25 +0100

    :)

--- sketch.js
+++ sketch.js
@@ -1,74 +1,82 @@
-let game = {
-	"status": 0,
-	"cores": [
-		{
-			"id": 3,
-			"team_id": 1,
-			"x": 0,
-			"y": 9,
-			"hp": 10000
-		},
-		{
-			"id": 4,
-			"team_id": 2,
-			"x": 9,
-			"y": 0,
-			"hp": 10000
-		}
-	],
-	"resources": [
-		{
-			"id": 5,
-			"value": 100,
-			"x": 1,
-			"y": 2,
-			"hp": 1000
-		},
-		{
-			"id": 6,
-			"value": 100,
-			"x": 3,
-			"y": 3,
-			"hp": 1000
-		}
-	],
-	"units": [
-		{
-			"id": 7,
-			"type_id": 10,
-			"hp": 100,
-			"x": 3,
-			"y": 4,
-			"team_id": 1,
-		},
-		{
-			"id": 8,
-			"type_id": 11,
-			"hp": 100,
-			"x": 1,
-			"y": 3,
-			"team_id": 1,
-		},
-		{
-			"id": 9,
-			"type_id": 10,
-			"hp": 100,
-			"x": 4,
-			"y": 1,
-			"team_id": 2,
-		}
-	],
-	"teams": [
-		{ 
-			"id": 1,
-			"balance": 250
-		},
-		{ 
-			"id": 2,
-			"balance": 150
-		}
-	]
-}
+// let game = {
+// 	"status": 0,
+// 	"cores": [
+// 		{
+// 			"id": 3,
+// 			"team_id": 1,
+// 			"x": 0,
+// 			"y": 9,
+// 			"hp": 10000
+// 		},
+// 		{
+// 			"id": 4,
+// 			"team_id": 2,
+// 			"x": 9,
+// 			"y": 0,
+// 			"hp": 10000
+// 		}
+// 	],
+// 	"resources": [
+// 		{
+// 			"id": 5,
+// 			"value": 100,
+// 			"x": 12,
+// 			"y": 0,
+// 			"hp": 1000
+// 		},
+// 		{
+// 			"id": 6,
+// 			"value": 100,
+// 			"x": 3,
+// 			"y": 3,
+// 			"hp": 1000
+// 		}
+// 	],
+// 	"units": [
+// 		{
+// 			"id": 7,
+// 			"type_id": 10,
+// 			"hp": 100,
+// 			"x": 3,
+// 			"y": 4,
+// 			"team_id": 1,
+// 		},
+// 		{
+// 			"id": 8,
+// 			"type_id": 11,
+// 			"hp": 100,
+// 			"x": 1,
+// 			"y": 3,
+// 			"team_id": 1,
+// 		},
+// 		{
+// 			"id": 9,
+// 			"type_id": 10,
+// 			"hp": 100,
+// 			"x": 4,
+// 			"y": 1,
+// 			"team_id": 2,
+// 		},
+// 		{
+// 			"id": 10,
+// 			"type_id": 11,
+// 			"hp": 100,
+// 			"x": 8,
+// 			"y": 3,
+// 			"team_id": 2,
+// 		}
+// 	],
+// 	"teams": [
+// 		{ 
+// 			"id": 1,
+// 			"balance": 250
+// 		},
+// 		{ 
+// 			"id": 2,
+// 			"balance": 150
+// 		}
+// 	]
+// }
 
 // let config = {
 // 	"width": 1000,
@@ -115,16 +123,17 @@ let game = {
 
 function preload() {
 	coreTexture = loadImage('minecraft_obsidian.jpeg');
-	dirtTexture = loadImage('light_gray_concrete.png');
+	dirtTexture = loadImage('light_gray_concrete-2.png');
 	goldTexture = loadImage('minecraft_gold.jpeg');
 	unit_miner1Texture = loadImage('miner1.png');
 	unit_miner2Texture = loadImage('miner2.png');
-	unit_worker1Texture = loadImage('worker1.png');
-	unit_worker2Texture = loadImage('worker2.png');
+	unit_worker1Texture = loadImage('Warrior_bot_red.png');
+	unit_worker2Texture = loadImage('Warrior_bot_blue.png');
 	soundFormats('mp3', 'ogg');
 	susSound = loadSound('among-us.mp3');
 	emergencySound = loadSound('emergency-meeting.mp3');
 	config = loadJSON('config.json');
+	game = loadJSON('State.json');
 }
 
 let cols;
@@ -147,7 +156,7 @@ function setup() {
 	button1.position(100, 100);
 	button1.mousePressed(emergency);
 	background(220);
-	text('tap here to play', 10, 20);
+	// text('tap here to play', 10, 20);
 	// setTimeout(() => {
 		// 	checkSounds();
 		// }, "10");
@@ -184,7 +193,8 @@ function get_texture(x, y) {
 		}
 	}
 
-	return dirtTexture;
+	// return dirtTexture;
+	return (0);
 }
 
 function sus() {
@@ -193,6 +203,41 @@ function sus() {
 
 function emergency() {
 	emergencySound.play();
+}
+
+function draw() {
+
+}
+
+function draw() {
+	// game.units[0].x = frameCount % rows;
+	boxSize = (windowWidth / rows) / 2;
+	background(150);
+	// rotateX(PI / 4);
+	// rotateZ(PI / 4);
+	// rotate(PI / 4);
+	// rotateY(PI / 4);
+
+  
+	for (let col = 0; col < cols; col++) {
+		for (let row = 0; row < rows; row++) {
+			let x = col * (boxSize);
+			let y = row * (boxSize);
+		
+			push();
+			translate(x - (cols - 1) * (boxSize) / 2, y - (rows - 1) * (boxSize) / 2, 0);
+			// rotateZ(-PI / 2);
+			texture(dirtTexture)
+			box(boxSize)
+			translate(0, 0, (boxSize / 2) + 1);
+			if (get_texture(col, row) != 0) {
+				texture(get_texture(col, row));
+				plane(boxSize);
+			}
+			pop();
+		}
+	}
+	orbitControl();
 }
 
 // function draw() {
@@ -222,7 +267,7 @@ function emergency() {
 // 			if (get_texture(col, row) != dirtTexture && get_texture(col, row) != coreTexture && get_texture(col, row) != goldTexture) {
 // 				// translate(0, 0, 1);
 // 				// plane();
-// 				// translate(0, 0, -1);
+// 				translate(0, 0, -1);
 // 				rotateX(PI / 2);
 // 				translate(0, boxSize / 2, 0);
 // 				rotateZ(PI);
@@ -285,38 +330,8 @@ function emergency() {
 // 	orbitControl();
 // }
 
-function draw() {
-	// game.units[0].x = frameCount % rows;
-	boxSize = (windowWidth / rows) / 2;
-	background(150);
-	// rotateX(PI / 4);
-	// rotateZ(PI / 4);
-	// rotate(PI / 4);
-			// rotateY(PI / 4);
-
-  
-	for (let col = 0; col < cols; col++) {
-		for (let row = 0; row < rows; row++) {
-			let x = col * (boxSize);
-			let y = row * (boxSize);
-		
-			push();
-			translate(x - (cols - 1) * (boxSize) / 2, y - (rows - 1) * (boxSize) / 2, 0);
-			// rotateZ(-PI / 2);
-			texture(dirtTexture)
-			box(boxSize)
-			translate(0, 0, (boxSize / 2) + 1);
-			texture(get_texture(col, row));
-			plane(boxSize);
-			pop();
-		}
-	}
-	orbitControl();
-
-}
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
 	boxSize = min(width, height) / max(cols, rows) - 10;
 }
-
